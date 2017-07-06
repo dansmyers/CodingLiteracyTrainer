86a003544639aa2c6fe143772944b3c1d1cc7c85
Dan S. Myers <dmyers@rollins.edu>
2017-07-05 22:07:27 -0400

    Update colt.js

--- js/colt.js
+++ js/colt.js
@@ -3,36 +3,36 @@
 
 //*** Constuctor ***//
 var CodingLiteracyTrainer = function() {
-	
-	// Constant parameters
-	this.MAX_ALLOWED_CHECKS = 2; // Num. checks before marking incorrect
-	this.REFRESH_DELAY = 1500;
-	
-	// Trainer state variables
-	this.currentProblem = null;
-		
+  
+  // Constant parameters
+  this.MAX_ALLOWED_CHECKS = 2; // Num. checks before marking incorrect
+  this.REFRESH_DELAY = 1500;
+  
+  // Trainer state variables
+  this.currentProblem = null;
+    
   // TODO: Add code to handle saved state
   // Load saved state from local storage
   // Use saved state if it's present
 
   // If saved state is not present, start with basic problem classes
-	this.model = new LearningModel();
-	
-	// Set up handler for use responses
-	var myself = this;  // Alias to this object
-	$('#submit').click(function() {
-		myself.checkAnswer();
-	});
+  this.model = new LearningModel();
+  
+  // Set up handler for use responses
+  var myself = this;  // Alias to this object
+  $('#submit').click(function() {
+    myself.checkAnswer();
+  });
 }
 
 
 //*** Generates the next problem after a short delay ***//
 CodingLiteracyTrainer.prototype.nextProblemAfterDelay = function() {
-	var trainer = this;
-	window.setTimeout(function() {
-		$('#response').html('');
-		trainer.nextProblem();
-	}, this.REFRESH_DELAY);
+  var trainer = this;
+  window.setTimeout(function() {
+    $('#response').html('');
+    trainer.nextProblem();
+  }, this.REFRESH_DELAY);
 }
 
 
@@ -51,43 +51,43 @@ CodingLiteracyTrainer.prototype.displayModal = function(unlocked) {
 // Effects: Outputs a correct/incorrect message to the interface.
 //   If the response is correct, load another problem.
 CodingLiteracyTrainer.prototype.checkAnswer = function() {
-	var answer = parseInt($('#answer').val());
-	
-	var problem = this.currentProblem;
-	problem.numTimesChecked += 1;
-	
-	// Correct
-	if (answer == problem.answer) {
-		$('#response').html('Correct!');
-		
-		for (var i = 0; i < problem.selectedFeatures.length; i++) {
-		    problem.selectedFeatures[i].correct += 1;
-	  }
-		
-		// Check for new unlocked features
-		var unlocked = this.model.checkForUnlockedFeatures();
-		if (unlocked > 0) {
-			this.displayModal(unlocked);
-		}
-		
-		this.nextProblemAfterDelay();
-	} 
-	
-	// Out of chances on this problem
-	else if (problem.numTimesChecked == this.MAX_ALLOWED_CHECKS) {
-		$('#response').html('Let\'s try another one.');
-		
-		for (var i = 0; i < problem.selectedFeatures.length; i++) {
-		    problem.selectedFeatures[i].incorrect += 1;
-	  } 
-		
-		this.nextProblemAfterDelay();
-	}
-	
-	// At least one chance remaining
-	else {
-		$('#response').html('Try again.');
-	}
+  var answer = parseInt($('#answer').val());
+  
+  var problem = this.currentProblem;
+  problem.numTimesChecked += 1;
+  
+  // Correct
+  if (answer == problem.answer) {
+    $('#response').html('Correct!');
+    
+    for (var i = 0; i < problem.selectedFeatures.length; i++) {
+        problem.selectedFeatures[i].correct += 1;
+    }
+    
+    // Check for new unlocked features
+    var unlocked = this.model.checkForUnlockedFeatures();
+    if (unlocked > 0) {
+      this.displayModal(unlocked);
+    }
+    
+    this.nextProblemAfterDelay();
+  } 
+  
+  // Out of chances on this problem
+  else if (problem.numTimesChecked == this.MAX_ALLOWED_CHECKS) {
+    $('#response').html('Let\'s try another one.');
+    
+    for (var i = 0; i < problem.selectedFeatures.length; i++) {
+        problem.selectedFeatures[i].incorrect += 1;
+    } 
+    
+    this.nextProblemAfterDelay();
+  }
+  
+  // At least one chance remaining
+  else {
+    $('#response').html('Try again.');
+  }
 }
 
 
@@ -98,44 +98,44 @@ CodingLiteracyTrainer.prototype.checkAnswer = function() {
 // Effects: writes the problem code, new problem question, and
 //   variable scratchpad to the interface
 CodingLiteracyTrainer.prototype.displayProblem = function(problem) {
-	// Insert the program into its text box
-	$('#program').text(problem.program);
+  // Insert the program into its text box
+  $('#program').text(problem.program);
 
-	// Display the question variable and its prompt
-	var questionVar = problem.questionVariable;
-	var questionString = 'What value is printed by the final line of'
-	                     + ' the program?'
-	$('#question').html(questionString);
-	
-	// Scratchpad of problem symbols
-	$('#scratchpad').html('');  // Clear current scratchpad
-	
-	var symbols = problem.symbols;	
-	for (var i = 0; i < symbols.length; i++) {
-			var htmlString = '<p><code>' + symbols[i] + ' = </code>' +      
-											'<input  type = "text"></input></p>';
-			$('#scratchpad').append(htmlString);
-	}
+  // Display the question variable and its prompt
+  var questionVar = problem.questionVariable;
+  var questionString = 'What value is printed by the final line of'
+                       + ' the program?'
+  $('#question').html(questionString);
+  
+  // Scratchpad of problem symbols
+  $('#scratchpad').html('');  // Clear current scratchpad
+  
+  var symbols = problem.symbols;  
+  for (var i = 0; i < symbols.length; i++) {
+      var htmlString = '<p><code>' + symbols[i] + ' = </code>' +      
+                      '<input  type = "text"></input></p>';
+      $('#scratchpad').append(htmlString);
+  }
 }
 
 
 //*** Generate and display a new random problem ***//
 CodingLiteracyTrainer.prototype.nextProblem = function () {
-				
-	// Generate a problem from the chosen class
-	// The resulting Problem object contains the result of generation
-	var problem = new Problem(this.model);
-	this.currentProblem = problem;
-	this.currentProblem.numTimesChecked = 0;
-	
-	// Update the interface
-	this.displayProblem(problem);
-	
-	// A handler for the user's response was already initialized in
-	// the CodingLiteracyTrainer constructor
+        
+  // Generate a problem from the chosen class
+  // The resulting Problem object contains the result of generation
+  var problem = new Problem(this.model);
+  this.currentProblem = problem;
+  this.currentProblem.numTimesChecked = 0;
+  
+  // Update the interface
+  this.displayProblem(problem);
+  
+  // A handler for the user's response was already initialized in
+  // the CodingLiteracyTrainer constructor
 }
-	
-	
+  
+  
 //*** Main ***//
 //
 // Constructs the CodingLiteracyTrainer object and generates the
@@ -143,10 +143,10 @@ CodingLiteracyTrainer.prototype.nextProblem = function () {
 //
 // Runs when the page loads
 function main() {
-	var trainer = new CodingLiteracyTrainer();
-	trainer.nextProblem();
+  var trainer = new CodingLiteracyTrainer();
+  trainer.nextProblem();
 }
-	
-	
+  
+  
 //*** Generates the first problem when the page is ready ***//
 $('document').ready(main);
